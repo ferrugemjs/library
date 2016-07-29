a1b26fde86d3916d028a8fb5bf437e8456ff694a
amebas <alex.query@gmail.com>
2016-07-29 13:36:45 -0500

    removendo dependencies

--- tasks/template-convert.js
+++ tasks/template-convert.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var replace = require('gulp-replace');
-var superviewsjs = require('gulp-superviewsjs');
+var ferrugemjs = require('gulp-ferrugemjs');
 
 var tsConfig = require("../tsconfig.json").compilerOptions;
 
@@ -10,18 +10,7 @@ gulp.task('template',function(){
     return gulp.src([
         "./src/**/*.html"
     ])
-    .pipe(replace(/(<template .+args=")(.+?)"/g,'$1$2 _$ferrugemRegister _$ferrugemLoad"'))
-    .pipe(replace('</require>', ''))
-    .pipe(replace(/<require from="([^"]*)">/g, '<script>_$ferrugemRegister.add("$1");</script>'))
-    .pipe(superviewsjs({mode:"amd"}))
-    .pipe(replace(/elementOpen\(("\w+?-[^"]+")([^)]+)\)/g,'_$ferrugemLoad.load($1$2).content(function(){'))
-    .pipe(replace(/elementClose\("\w+?-+\w.+\)+?/g,'});'))
-    .pipe(replace('elementClose("content")',''))
-    .pipe(replace('elementOpen("content")','this.content();'))
-    
-    //.pipe(replace(/elementClose\("\w+?-+\w.+\)+?/g,'elementClose("div")'))    
-    //.pipe(replace(/elementOpen\(("\w+?-[^"]+")([^)]+)\)/g,'elementOpen("div"$2);_$ferrugemLoad.load($1$2);'))
-    //.pipe(replace(/elementOpen(\("\w+?-+\w.+\)+?)/g,'elementOpen$1;_$ferrugemLoad.load$1;'))
+    .pipe(ferrugemjs({mode:"amd"}))
     .pipe(rename({
         extname: ".html.js"
     }))
