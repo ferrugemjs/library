89fd89ed931a9c82fec712d176a91764111b879c
ferrugemjs <ferrugemjavascript@gmail.com>
2017-01-23 12:24:59 -0300

    test with respect id function

--- src/example/compost-comp.ts
+++ src/example/compost-comp.ts
@@ -4,12 +4,14 @@ export class CompostComp{
 	private onEventMsg1:{a:string}={a:'sei nao cabra'};
 	private myfnext:Function;
 	private onEventMsg:{a:Function};
+	private id:number;
 	constructor(){
 		this.especialname = "humm-default";
 		this.onEventMsg={a:(fnext:Function)=>{
 			//fnext();
 			this.myfnext=fnext;
 		}};
+		this.id = 2010;
 	}
 	setEspecialname2(n:string):void{
 		console.log(n);
