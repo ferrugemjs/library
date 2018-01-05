b8c6a769fae4ffafd0a4e4f0cefc2e712d89d58b
ferrugemjs <ferrugemjavascript@gmail.com>
2018-01-04 21:06:53 -0300

    fixed bootstrapper template extension parameter

--- src/core/bootstrapper.ts
+++ src/core/bootstrapper.ts
@@ -14,19 +14,19 @@ if(app_html_s.length === 0){
 app_html_s = null;
 
 let app_url:string = app_html.getAttribute("app")||"app";
-let app_template_extension = app_html.getAttribute("template-extension") || ".html";
+let templateExtension = app_html.getAttribute("template-extension") || ".html";
 
 
 if(typeof __webpack_require__ === 'function'){
-	require([`root_app/${app_url}${app_template_extension}`],(_mod_init_app:any) => {
+	require([`root_app/${app_url}${templateExtension}`],(_mod_init_app:any) => {
 	platform
-		.bootstrap(_mod_init_app)
+		.bootstrap(_mod_init_app,{templateExtension})
 		.at(app_html);
 	});
 }else{
-	require([`${app_url}${app_template_extension}`],(_mod_init_app:any) => {
+	require([`${app_url}${templateExtension}`],(_mod_init_app:any) => {
 	platform
-		.bootstrap(_mod_init_app)
+		.bootstrap(_mod_init_app,{templateExtension})
 		.at(app_html);
 	});
 }
