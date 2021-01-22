05ebfd80d5ac92cdbf643685b3b367e7edf2519f
ferrugemjs <ferrugemjavascript@gmail.com>
2021-01-22 16:09:49 -0300

    removing bootstrap

--- src/core/platform.ts
+++ src/core/platform.ts
@@ -3,19 +3,13 @@ import { patch } from 'incremental-dom';
 
 export class PlatformBootstrap {
   public bootstrap(pmodule: any, option?: { templateExtension: string }): any {
-    // componentFactory.config.templateExtension = option && option.templateExtension ? option.templateExtension : '.html';
     return { at:(domRender: HTMLElement) => this.at(domRender,pmodule) };
   }
   public at(domRender: HTMLElement, pmodule?: any): void {
-    //let key_id: string = domRender.getAttribute('id');
-    //if (!key_id) {
     const key_id = `uid_${new Date().getTime()}`;
-      //domRender.setAttribute('id', key_id);
-    //}
     const is = 'init-app-tag';
-    const proxyInst = componentFactory(pmodule.default,{z: 1000},{is, key_id}).content(function(){});
+    const proxyInst = componentFactory(pmodule,{z: 1000},{is, key_id}).content(function(){});
     patch(domRender, proxyInst.$render.bind(proxyInst,{is, key_id}), proxyInst);
-    //patch(domRender, proxy_inst.$render.bind(proxy_inst,{key_id, is, loaded:true}), proxy_inst);
   }
 }
 
