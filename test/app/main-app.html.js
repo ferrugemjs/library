9d8405cb422309451ee8d869d8641b87c5d54096
ferrugemjs <ferrugemjavascript@gmail.com>
2023-02-21 13:39:11 -0300

    simplify path

--- test/app/main-app.html.js
+++ test/app/main-app.html.js
@@ -1,14 +1,14 @@
-define(["exports","incremental-dom","@ferrugemjs/library/dist/core/component-factory","./main-app"], function (exports,_idom,_libfjs_factory,_main_app){
+define(["exports","incremental-dom","@ferrugemjs/library/component-factory","./main-app"], function (exports,_idom,_libfjs_factory,_main_app){
 	var __main_app_tmp = Object.keys(_main_app)[0];
 	exports.default = (function(super_clazz){
-		function _clazz_sub_f_d60b5d62_2c5c_4e78_9889_f75a30aa5f89_tmp(props){
+		function _clazz_sub_f_43b5d4ef_2ea0_40ba_83c7_46b7af1d37bf_tmp(props){
 			if(super_clazz.call){
 				super_clazz.call(this, props);
 			}
 		};
-		_clazz_sub_f_d60b5d62_2c5c_4e78_9889_f75a30aa5f89_tmp.prototype = Object.create(super_clazz.prototype || super_clazz);
-		_clazz_sub_f_d60b5d62_2c5c_4e78_9889_f75a30aa5f89_tmp.prototype.constructor = _clazz_sub_f_d60b5d62_2c5c_4e78_9889_f75a30aa5f89_tmp;
-		_clazz_sub_f_d60b5d62_2c5c_4e78_9889_f75a30aa5f89_tmp.prototype.$render = function(config_props){if(!config_props.loaded){ _idom.elementOpen("div",""+(config_props.key_id)+"",[""],"is",(config_props.is),"id",(config_props.key_id));
+		_clazz_sub_f_43b5d4ef_2ea0_40ba_83c7_46b7af1d37bf_tmp.prototype = Object.create(super_clazz.prototype || super_clazz);
+		_clazz_sub_f_43b5d4ef_2ea0_40ba_83c7_46b7af1d37bf_tmp.prototype.constructor = _clazz_sub_f_43b5d4ef_2ea0_40ba_83c7_46b7af1d37bf_tmp;
+		_clazz_sub_f_43b5d4ef_2ea0_40ba_83c7_46b7af1d37bf_tmp.prototype.$render = function(config_props){if(!config_props.loaded){ _idom.elementOpen("div",""+(config_props.key_id)+"",[""],"is",(config_props.is),"id",(config_props.key_id));
 
 	
 };				
@@ -24,6 +24,6 @@ _idom.text(""+(this.msg)+"");
 
 	_idom.elementClose("span");
 		if(!config_props.loaded){_idom.elementClose("div");};	};
-		return _clazz_sub_f_d60b5d62_2c5c_4e78_9889_f75a30aa5f89_tmp;
+		return _clazz_sub_f_43b5d4ef_2ea0_40ba_83c7_46b7af1d37bf_tmp;
 	})(_main_app[__main_app_tmp] || _main_app);
 });
